dc7315ebdc64728e82ccb8bff8b7711b6fcd0857
pkovalski <pricila.kovalski@tools.com.br>
2019-12-27 15:48:10 -0300

    agenda

---
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Kovalski","Vega","Boulos","Alfredo"));
+
+        ListView listaDeAlunos = findViewById(R.id.activity_main_Lista_de_Alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+
+
+    }
+}
